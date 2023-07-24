559262a7945bb24f2bf4cc3b4ae56724ec0e32c6
jnjerin <njerijoan24@gmail.com>
2023-07-24 23:52:51 +0300

    initialized  base class attributes

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,22 @@ from models import storage
 
 
 class BaseModel:
-    """Defines all common attributes/methods
+    """Base class that defines all common attributes/methods
     """
     def __init__(self, *args, **kwargs):
+        """initializes all attributes
+        """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
+        else:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)            
+
+
